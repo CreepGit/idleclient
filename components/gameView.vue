90079aff40bc491837d8aa4030a1289f4bc0571a
Creep <54237803+CreepGit@users.noreply.github.com>
2024-08-24 14:17:33 +0300

    further inv

--- components/gameView.vue
+++ components/gameView.vue
@@ -6,8 +6,19 @@
                 <br>
                 <Slider v-model="width" style="max-width: 500px;" />
                 <br>
+                <Button style="margin: 5px;" @click="shuffle" label="Shuffle" />
+                <Button style="margin: 5px;" @click="sortCount" label="Sort Count" />
+                <Button style="margin: 5px;" @click="sortName" label="Sort Name" />
+                <br>
+                <br>
                 <div class="invGrid" :style="{width: width*5+'px'}">
-                    <InventoryItem v-for="_ in getItemCount()" :img="getRandomSprite()" :count="getCountExpotential()" :rarity="getRarity()" />
+                    <TransitionGroup name="list">
+                        <template v-for="index in 28">
+                            <InventoryItem @delete="deleteValue(index-1)" v-if="item=inventory[index-1]" :name="item.name" :key="item.icon" :img="'sprite/'+item.icon" :count="item.count" :rarity="item.rarity" />
+                            <div v-else class="emptySlot" :key="index"></div>
+                        </template>
+                        <!-- <InventoryItem v-for="item, index in inventory" :key="item.icon" :img="'sprite/'+item.icon" :count="item.count" :rarity="item.rarity" /> -->
+                    </TransitionGroup>
                 </div>
             </template>
         </Card>
@@ -42,7 +53,55 @@ const sprites = [
     "/sprite/tile023.png",
 ]
 
+const inventory = ref<({name: string, icon: string, count: number, rarity: 'normal'|'rare'|'epic'|'legendary'}|null)[]>([
+    { name: 'Pack', icon: "tile000.png", count: getCountExpotential(), rarity: "normal" },
+    { name: 'Explosive', icon: "tile001.png", count: getCountExpotential(), rarity: "rare" },
+    { name: 'Bucket', icon: "tile002.png", count: getCountExpotential(), rarity: "epic" },
+    { name: 'Rope', icon: "tile003.png", count: getCountExpotential(), rarity: "legendary" },
+    { name: 'Spyglass', icon: "tile004.png", count: getCountExpotential(), rarity: "normal" },
+    { name: 'Map', icon: "tile005.png", count: getCountExpotential(), rarity: "rare" },
+    { name: 'Pickaxe', icon: "tile006.png", count: getCountExpotential(), rarity: "epic" },
+    { name: 'Shovel', icon: "tile007.png", count: getCountExpotential(), rarity: "legendary" },
+    { name: 'Fork', icon: "tile008.png", count: getCountExpotential(), rarity: "normal" },
+    { name: 'Axe', icon: "tile009.png", count: getCountExpotential(), rarity: "rare" },
+    { name: 'Sword', icon: "tile010.png", count: getCountExpotential(), rarity: "epic" },
+    { name: 'Fishing Rod', icon: "tile011.png", count: getCountExpotential(), rarity: "legendary" },
+    { name: 'Bow', icon: "tile012.png", count: getCountExpotential(), rarity: "normal" },
+    { name: 'Stick', icon: "tile013.png", count: getCountExpotential(), rarity: "rare" },
+    { name: 'Torch', icon: "tile014.png", count: getCountExpotential(), rarity: "epic" },
+    { name: 'Compass', icon: "tile015.png", count: getCountExpotential(), rarity: "legendary" },
+    { name: 'Mana Potion', icon: "tile016.png", count: getCountExpotential(), rarity: "normal" },
+    { name: 'Health Potion', icon: "tile017.png", count: getCountExpotential(), rarity: "rare" },
+    { name: 'Crystal', icon: "tile018.png", count: getCountExpotential(), rarity: "epic" },
+    { name: 'Gem', icon: "tile019.png", count: getCountExpotential(), rarity: "legendary" },
+    { name: 'Gold', icon: "tile020.png", count: getCountExpotential(), rarity: "normal" },
+    { name: 'Clay', icon: "tile021.png", count: getCountExpotential(), rarity: "rare" },
+    { name: 'Stone', icon: "tile022.png", count: getCountExpotential(), rarity: "epic" },
+    { name: 'Coal', icon: "tile023.png", count: getCountExpotential(), rarity: "legendary" },
+])
+
+const item: any = undefined
 const width = ref(50)
+
+function shuffle() {
+    const toAdd = [...inventory.value]
+    inventory.value = toAdd.sort(() => Math.random() - 0.5)
+}
+
+function sortCount() {
+    const toAdd = [...inventory.value]
+    inventory.value = toAdd.sort((a: any, b: any) => a.count - b.count)
+}
+
+function sortName() {
+    const toAdd = [...inventory.value]
+    inventory.value = toAdd.sort((a: any, b: any) => a.name.localeCompare(b.name))
+}
+
+function deleteValue(index: number) {
+    const toAdd = [...inventory.value].filter((_, i) => i !== index)
+    inventory.value = toAdd
+}
 
 function getRandomSprite() {
     const randomIndex = Math.floor(Math.random() * sprites.length)
@@ -73,7 +132,7 @@ function getRarity() {
     display: grid;
     max-width: 690px;
     // border: 1px solid black;
-    grid-template-columns: repeat(auto-fill, 60px);
+    grid-template-columns: repeat(auto-fill, 50px);
     user-select: none;
     justify-content: center;
     gap: 5px;
@@ -86,5 +145,22 @@ function getRarity() {
         background-color: black;
         top: calc(50% - 10px);
     }
+}
+.list-move,
+.list-enter-active,
+.list-leave-active {
+  transition: all 200ms ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+.emptySlot {
+    width: 50px;
+    height: 50px;
+    border-radius: 15px;
+    background-color: #0003;
+    box-shadow: inset 0 0 20px oklch(35% 0% 60deg);
 }
 </style>
--- components/inventoryItem.vue
+++ components/inventoryItem.vue
@@ -1,32 +1,43 @@
 <template>
-    <div class="card" :class="rarity" @contextmenu.prevent="e=>menu.show(e)">
-        <img :src="img" alt="" aria-haspopup="true" aria-controls="overlay_menu" >
+    <div class="card" :class="rarity" @contextmenu.prevent="e=>{tooltip.hide(e);menu.show(e)}" @mouseenter="e=>tooltip.show(e)" @mouseleave="e=>tooltip.hide(e)">
+        <img :src="img" alt="" aria-haspopup="true" aria-controls="overlay_menu tooltip" >
         <span class="count">{{ props.count }}</span>
         <Menu ref="menu" :model="menuItems" :popup="true" id="overlay_menu" />
+        <Popover ref="tooltip" :popup="true" id="tooltip" style="pointer-events: none;">
+            <div>
+                <h4>{{ props.name }}</h4>
+            </div>
+        </Popover>
     </div>
 </template>
 
 <script setup lang="ts">
+import type { MenuItem } from 'primevue/menuitem';
+
 const props = defineProps<{
     count: number,
     img: string,
-    rarity: "normal" | "rare" | "epic" | "legendary"
+    rarity: "normal" | "rare" | "epic" | "legendary",
+    name: string,
 }>()
 
+const emit = defineEmits(['delete'])
+
 const menu = ref<any|undefined>(undefined)
-const menuItems = [
+const tooltip = ref<any|undefined>(undefined)
+const menuItems: MenuItem[] = [
     { label: 'Use', icon: 'pi pi-fw pi-check' },
     { label: 'Sell', icon: 'pi pi-fw pi-dollar' },
     { label: 'Info', icon: 'pi pi-fw pi-info' },
-    { label: 'Delete', icon: 'pi pi-fw pi-trash' }
+    { label: 'Delete', icon: 'pi pi-fw pi-trash', command: ()=>emit('delete') },
 ]
 </script>
 
 
 <style lang="scss" scoped>
 .card {
-    width: 60px;
-    height: 60px;
+    width: 50px;
+    height: 50px;
     border-radius: 15px;
     transition: box-shadow 150ms;
     display: inline-block;
@@ -65,6 +76,9 @@ const menuItems = [
     background-color: #0009;
     padding: 0.1em 0.3em;
     border-radius: 0.4em;
+}
+.tooltip {
+    display: none !important;
 }
 </style>
 
